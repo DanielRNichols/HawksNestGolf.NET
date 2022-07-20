75cfe8b48330f05f99543a77bceadb2d2917a824
DanielRNichols <danielrnichols63@gmail.com>
2022-07-19 22:44:48 -0500

    Adds RowActions

---
+++ Client/Models/RowAction.cs
@@ -0,0 +1,10 @@
+﻿namespace HawksNestGolf.NET.Client.Models
+{
+    public class RowAction<T>
+    {
+        public string Icon { get; set; } = string.Empty;
+        public string DisplayLabel { get; set; } = string.Empty;
+        public string Tooltip { get; set; } = string.Empty;
+        public Action<T>? Action { get; set; }
+    }
+}
--- Client/Models/TableDefinition.cs
+++ Client/Models/TableDefinition.cs
@@ -5,10 +5,13 @@
         public IList<ColumnDefinition<T>> Columns { get; set; } = new List<ColumnDefinition<T>>();
         public IList<T>? Items { get; set; } = null;
 
-        public TableDefinition(IList<ColumnDefinition<T>> columns, IList<T>? items)
+        public IList<RowAction<T>>? RowActions { get; set; } = new List<RowAction<T>>();
+
+        public TableDefinition(IList<ColumnDefinition<T>> columns, IList<T>? items, IList<RowAction<T>>? rowActions = null)
         {
             Columns = columns;
             Items = items;
+            RowActions = rowActions;
         }
     }
 }
