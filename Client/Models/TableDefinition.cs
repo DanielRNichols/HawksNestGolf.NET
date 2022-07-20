488060520a4d00f82f6502b8a72fb9eb82ef9a82
DanielRNichols <danielrnichols63@gmail.com>
2022-07-19 21:59:31 -0500

    Adds TableDefinition

---
+++ Client/Models/TableDefinition.cs
@@ -0,0 +1,14 @@
+﻿namespace HawksNestGolf.NET.Client.Models
+{
+    public class TableDefinition<T>
+    {
+        public IList<ColumnDefinition<T>> Columns { get; set; } = new List<ColumnDefinition<T>>();
+        public IList<T>? Items { get; set; } = null;
+
+        public TableDefinition(IList<ColumnDefinition<T>> columns, IList<T>? items)
+        {
+            Columns = columns;
+            Items = items;
+        }
+    }
+}
