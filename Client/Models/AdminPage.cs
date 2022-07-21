445a0e8639d52e7140087fae3b90c91eda54c5be
DanielRNichols <danielrnichols63@gmail.com>
2022-07-20 20:19:06 -0500

    Adds AdminPage as base page

---
+++ Client/Models/AdminPage.cs
@@ -0,0 +1,10 @@
+﻿namespace HawksNestGolf.NET.Client.Models
+{
+    public class AdminPage<T>
+    {
+        public string Header { get; set; } = string.Empty;
+        public string Title { get; set; } = string.Empty;
+        public TableDefinition<T>? TableDefinition { get; set; }
+
+    }
+}
--- Client/Models/TableDefinition.cs
+++ Client/Models/TableDefinition.cs
@@ -6,18 +6,5 @@
         public IList<T>? Items { get; set; } = null;
         public IList<RowAction<T>>? RowActions { get; set; } = new List<RowAction<T>>();
         public IList<TableAction<T>>? TableActions { get; set; } = new List<TableAction<T>>();
-
-        public TableDefinition(
-            IList<ColumnDefinition<T>> columns, 
-            IList<T>? items, 
-            IList<RowAction<T>>? rowActions = null,
-            IList<TableAction<T>>? tableActions = null
-            )
-        {
-            Columns = columns;
-            Items = items;
-            RowActions = rowActions;
-            TableActions = tableActions;
-        }
     }
 }
